433b9caa6a0f688ff04ae0c8ade2b20f0a306c1f
saiefsmairi <saieftaher@gmail.com>
2022-06-13 00:53:38 +0100

    fix profil responsiveness

--- src/pages/dashboard/Myprofil.jsx
+++ src/pages/dashboard/Myprofil.jsx
@@ -5,6 +5,9 @@ import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import { InputLabel } from '@mui/material';
 import './DashboardStyle/Myprofil.css';
+import Typography from '@mui/material/Typography';
+import ButtonBase from '@mui/material/ButtonBase';
+import MonthlyBarChart from './MonthlyBarChart';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -14,127 +17,128 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
 }));
 
-
+const Img = styled('img')({
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+});
 export default function Myprofil() {
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2} direction="column">
-                <Grid item xs={12} md={12}>
-                    <h3>User information</h3>
+            <Grid container spacing={2}>
+
+                <Grid item xs={12} md={8} sm container>
+                    <Grid item xs container direction="column" spacing={2}>
+
+                        <Grid item xs container spacing={2}>
+                            <Grid item xs={12} md={12}>
+                                <h4>USER INFORMATION</h4>
+                            </Grid>
+                            <Grid item xs={6} md={5}>
+                                <InputLabel style={{
+                                    fontWeight: 'bold'
+                                }} >First Name</InputLabel>
+                                <TextField fullWidth id="outlined-basic" variant="outlined" />
+                            </Grid>
+
+                            <Grid item xs={6} md={5}>
+                                <InputLabel style={{
+                                    fontWeight: 'bold'
+                                }} >Last Name</InputLabel>
+                                <TextField fullWidth id="outlined-basic" variant="outlined" />
+                            </Grid>
+                        </Grid>
+
+                        <Grid item xs container spacing={2}>
+
+                            <Grid item xs={6} md={5}>
+                                <InputLabel style={{
+                                    fontWeight: 'bold'
+                                }} >Phone Number</InputLabel>
+                                <TextField fullWidth id="outlined-basic" variant="outlined" />
+                            </Grid>
+
+                            <Grid item xs={6} md={5}>
+                                <InputLabel style={{
+                                    fontWeight: 'bold'
+                                }} >Email Adress</InputLabel>
+                                <TextField fullWidth id="outlined-basic" variant="outlined" />
+                            </Grid>
+                        </Grid>
+
+                        <Grid item xs container spacing={2}>
+                            <Grid item xs={12} md={10}>
+                                <hr style={{
+
+                                    height: .5,
+                                    borderColor: 'rgb(239 238 238)'
+                                }} />
+                            </Grid>
+
+                        </Grid>
+                        <Grid item xs container spacing={2}>
+                            <Grid item xs={12} md={12}>
+                                <h4>CONTACT INFORMATION</h4>
+                            </Grid>
+                            <Grid item xs={12} md={10}>
+                                <InputLabel style={{
+                                    fontWeight: 'bold'
+                                }} >Adress</InputLabel>
+                                <TextField fullWidth id="outlined-basic" variant="outlined" />
+                            </Grid>
+                            <Grid item xs={6} md={"auto"}>
+                            </Grid>
+
+                        </Grid>
+
+                        <Grid item xs container spacing={2}>
+
+                            <Grid item xs={4} md={3.3}>
+                                <InputLabel style={{
+                                    fontWeight: 'bold'
+                                }} >City</InputLabel>
+                                <TextField fullWidth id="outlined-basic" variant="outlined" />
+
+                            </Grid>
+                            <Grid item xs={4} md={3.3}>
+                                <InputLabel style={{
+                                    fontWeight: 'bold'
+                                }} >Country</InputLabel>
+                                <TextField fullWidth id="outlined-basic" variant="outlined" />
+
+                            </Grid>
+                            <Grid item xs={4} md={3.3}>
+                                <InputLabel style={{
+                                    fontWeight: 'bold'
+                                }} >Postal code</InputLabel>
+                                <TextField fullWidth id="outlined-basic" variant="outlined" />
+                            </Grid>
+                        </Grid>
+
+                        <Grid item xs container spacing={2}>
+                            <Grid item xs={12} md={10}>
+                                <hr style={{
+
+                                    height: .5,
+                                    borderColor: 'rgb(239 238 238)'
+                                }} />
+                            </Grid>
+
+                        </Grid>
+                    </Grid>
                 </Grid>
+                <Grid item md={4}>
+                    <Box sx={{ p: 3, pb: 0 }}>
 
-                <Grid item xs container spacing={2}>
+                        <Typography variant="h6" color="textSecondary">
+                            This Week Statistics
+                        </Typography>
+                        <Typography variant="h4">$7,650</Typography>
 
-                    <Grid item xs={6} md={3}>
-                        <InputLabel style={{
-                            fontWeight: 'bold'
-                        }} >First Name</InputLabel>
-                        <TextField fullWidth id="outlined-basic" variant="outlined" />
-                    </Grid>
-                    <Grid item xs={6} md={"auto"}>
-                    </Grid>
-                    <Grid item xs={6} md={3}>
-                        <InputLabel style={{
-                            fontWeight: 'bold'
-                        }} >Last Name</InputLabel>
-                        <TextField fullWidth id="outlined-basic" variant="outlined" />
-                    </Grid>
+                    </Box>
+                    <MonthlyBarChart />
                 </Grid>
-
-                <Grid item xs container spacing={2}>
-
-                    <Grid item xs={6} md={3}>
-                        <InputLabel style={{
-                            fontWeight: 'bold'
-                        }} >Phone Number</InputLabel>
-                        <TextField fullWidth id="outlined-basic" variant="outlined" />
-
-                    </Grid>
-                    <Grid item xs={6} md={"auto"}>
-                    </Grid>
-                    <Grid item xs={6} md={3}>
-                        <InputLabel style={{
-                            fontWeight: 'bold'
-                        }} >Email Adress</InputLabel>
-                        <TextField fullWidth id="outlined-basic" variant="outlined" />
-                    </Grid>
-                </Grid>
-
-                <Grid item xs container spacing={2}>
-
-                    <Grid item xs={6} md={6}>
-
-                        <hr style={{
-
-                            height: .5,
-                            borderColor: 'rgb(239 238 238)'
-                        }} />
-
-
-                    </Grid>
-
-                </Grid>
-
-
-            </Grid>
-
-            <Grid container spacing={2} direction="column">
-                <Grid item xs={12} md={12}>
-                    <h3>Contact information</h3>
-                </Grid>
-
-                <Grid item xs container spacing={2}>
-
-                
-                
-                    <Grid item xs={6} md={6}>
-                        <InputLabel style={{
-                            fontWeight: 'bold'
-                        }} >Adress</InputLabel>
-                        <TextField fullWidth id="outlined-basic" variant="outlined" />
-                    </Grid>
-                </Grid>
-
-                <Grid item xs container spacing={2}>
-
-                    <Grid item xs={2} md={2}>
-                        <InputLabel style={{
-                            fontWeight: 'bold'
-                        }} >City</InputLabel>
-                        <TextField fullWidth id="outlined-basic" variant="outlined" />
-
-                    </Grid>
-                    <Grid item xs={2} md={2}>
-                        <InputLabel style={{
-                            fontWeight: 'bold'
-                        }} >Country</InputLabel>
-                        <TextField fullWidth id="outlined-basic" variant="outlined" />
-
-                    </Grid>
-                    <Grid item xs={2} md={2}>
-                        <InputLabel style={{
-                            fontWeight: 'bold'
-                        }} >Postal code</InputLabel>
-                        <TextField fullWidth id="outlined-basic" variant="outlined" />
-                    </Grid>
-                </Grid>
-
-                <Grid item xs container spacing={2}>
-
-                    <Grid item xs={6} md={6}>
-
-                        <hr style={{
-
-                            height: .5,
-                            borderColor: 'rgb(239 238 238)'
-                        }} />
-
-
-                    </Grid>
-
-                </Grid>
-
-
             </Grid>
 
 
